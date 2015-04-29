a70c1eb9c029026fd1c8f6642af11e072707f9e6
Varun Naik <varun.naik@gmail.com>
2015-04-29 13:43:52 +1000

    Parse JSON and send response.

--- main.js
+++ main.js
@@ -1,11 +1,40 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 
+app.use(bodyParser.json());
+
+app.use (function (error, request, response, next){
+    response.status(406).send('Invalid JSON');
+});
+
 app.set('port', (process.env.PORT || 3000));
-app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.send('Please Post data instead.');
+});
+
+app.post('/', function(request, response) {
+    // Parse the incoming data
+    var json = request.body;
+    var matchedShows = [];
+
+    if (!json.payload) {
+        response.status(406).send('Could not parse; Unexpected JSON in input.');
+    }
+
+    json.payload.forEach(function(show) {
+        if (show.drm) {
+            if (show.episodeCount > 0) {
+                matchedShows.push({
+                    "image": show.image.showImage,
+                    "slug": show.slug,
+                    "title": show.title
+                });
+            }
+        }
+    });
+    response.json({"response": matchedShows}).end();
 });
 
 app.listen(app.get('port'), function() {
